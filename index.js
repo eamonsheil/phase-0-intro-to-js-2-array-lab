6302f386cd3c0ca338d2d0c07c7f41fcda1f3278
Eamon Sheil <Eamondsheil@gmail.com>
2022-01-27 09:50:17 -0500

    commit tree 748bce33eaf7104a991e14c7b77c35931ebc911c

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');    
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats, "Broom"];
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    const newerCat = ["Arnold", ...cats];
+    return newerCat;
+}
+
+function removeLastCat(){
+    const copyCats = cats.slice(0, cats.length -1);
+    return copyCats;
+}
+
+function removeFirstCat() {
+    const copiousCats = cats.slice(1);  
+    return copiousCats;
+}
